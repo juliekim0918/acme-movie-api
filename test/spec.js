9b6535c609d398be4cc3c73c27123ecd0a346cd9
Julie Kim <juliekim0918@gmail.com>
2022-02-02 20:46:10 -0500

    test out express route

--- test/spec.js
+++ test/spec.js
@@ -1,5 +1,6 @@
 const mocha = require("mocha");
 const { expect } = require("chai");
+const app = require("supertest")(require("../app"));
 
 describe("my app", () => {
   describe("first tests", () => {
@@ -8,6 +9,13 @@ describe("my app", () => {
     });
     it("bar equals to bar", () => {
       expect("bar").to.equal("bar");
+    });
+  });
+
+  describe("my application", () => {
+    it("GET / returns a success status", async () => {
+      const response = await app.get("/");
+      expect(response.status).to.equal(200);
     });
   });
 });
