efd0e6a2c2860fc04dd4e2d838f99b4f060b675a
Julie Kim <juliekim0918@gmail.com>
2022-02-02 20:38:51 -0500

    new build

--- test/spec.js
+++ test/spec.js
@@ -1,9 +1,13 @@
+const mocha = require("mocha");
 const { expect } = require("chai");
 
 describe("my app", () => {
   describe("first tests", () => {
     it("foo equals to foo", () => {
       expect("foo").to.equal("foo");
+    });
+    it("bar equals to bar", () => {
+      expect("bar").to.equal("bar");
     });
   });
 });
